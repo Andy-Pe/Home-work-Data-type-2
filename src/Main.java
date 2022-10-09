92ea4e1d96e7620f5695389b92cc07cf16a3518c
Andrey Petrov <Pandand1@yandex.ru>
2022-10-09 13:21:27 +0300

    making edits

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,16 @@ public class Main {
     public static void main(String[] args) {
         int balance = 1000;
         int payment = 3200;
-        boolean upThousand = payment>1000;
+        boolean upThousand = payment > 1000;
         int bonus;
         if (upThousand) {
             bonus = payment / 100;
         } else {
             bonus = 0;
         }
-
+        System.out.println("Итоговая сумма на счёте:");
         System.out.println(balance + payment + bonus);
+        System.out.println("Начисленные бонусы:");
         System.out.println(bonus);
 
     }
